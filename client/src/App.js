40cdbaf1f0345bda443ea7f04b49726ca109f919
Mostafa Ghanbari <mostafa.qanbary@gmail.com>
2020-10-20 18:34:32 -0400

    Setuped bootstrap using bootswatch and React-bootstrap. Created Header and footer using react-bootstrap

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,18 @@
 import React from 'react'
+import { Container } from 'react-bootstrap'
+import Header from './components/layouts/Header'
+import Footer from './components/layouts/Footer'
 
 const App = () => {
-  return <>Hello world</>
+  return (
+    <>
+      <Header />
+      <Container>
+        <main>Welcome to our shop</main>
+      </Container>
+      <Footer />
+    </>
+  )
 }
 
 export default App
---
+++ client/src/components/layouts/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Container } from 'react-bootstrap'
+
+const Header = () => {
+  return (
+    <header>
+      <Container>Header</Container>
+    </header>
+  )
+}
+
+export default Header
