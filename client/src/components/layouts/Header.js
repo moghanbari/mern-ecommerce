1d00c1a8b5b7e227d5cd2efdc617640f17e556e1
Mostafa Ghanbari <mostafa.qanbary@gmail.com>
2020-10-20 19:20:22 -0400

    Made the navbar and sent footer to the bottom by expanding main tag

--- client/src/components/layouts/Header.js
+++ client/src/components/layouts/Header.js
@@ -1,10 +1,21 @@
 import React from 'react'
-import { Container } from 'react-bootstrap'
+import { Container, Navbar, Nav } from 'react-bootstrap'
 
 const Header = () => {
   return (
     <header>
-      <Container>Header</Container>
+      <Navbar bg='primary' variant='dark' expand='lg' collapseOnSelect>
+        <Container>
+          <Navbar.Brand href='/'>MERN Shop</Navbar.Brand>
+          <Navbar.Toggle aria-controls='basic-navbar-nav' />
+          <Navbar.Collapse id='basic-navbar-nav'>
+            <Nav className='ml-auto'>
+              <Nav.Link href='/'>Home</Nav.Link>
+              <Nav.Link href='/login'>Sign in</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </header>
   )
 }
