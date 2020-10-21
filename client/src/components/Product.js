f21ac689db915e827bc8ac63a47add5e36c37a9a
Mostafa Ghanbari <mostafa.qanbary@gmail.com>
2020-10-21 18:14:02 -0400

    Installed react-router-dom and bootstrap for react-router. Implemented first router. Minor add in css

--- client/src/components/Product.js
+++ client/src/components/Product.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Card } from 'react-bootstrap'
+import Rating from './Rating'
 
 const Product = ({ product }) => {
   return (
@@ -16,9 +17,7 @@ const Product = ({ product }) => {
         </a>
 
         <Card.Text as='div'>
-          <div className='my-3'>
-            {product.rating} from {product.numReviews}
-          </div>
+          <Rating value={product.rating} reviews={product.numReviews} />
           <Card.Text as='h5'>${product.price}</Card.Text>
         </Card.Text>
       </Card.Body>
