6d8006e4b70b07d6d247bc786e7dfd897e474d05
Mostafa Ghanbari <mostafa.qanbary@gmail.com>
2020-10-22 00:00:18 -0400

    Implemented products detail

--- client/src/screens/ProductScreen.js
+++ client/src/screens/ProductScreen.js
@@ -12,6 +12,62 @@ const ProductScreen = ({ match }) => {
       <Link className='btn btn-light my-3' to='/'>
         Go Back
       </Link>
+
+      <Row>
+        <Col md={6}>
+          <Image src={product.image} alt={product.name} fluid />
+        </Col>
+        <Col md={5}>
+          <Card className='border-0'>
+            <ListGroup variant='flush'>
+              <ListGroup.Item>
+                <h5>{product.name}</h5>
+              </ListGroup.Item>
+
+              <ListGroup.Item>
+                <Rating value={product.rating} reviews={product.numReviews} />
+              </ListGroup.Item>
+
+              <ListGroup.Item>
+                <Row>
+                  <Col>Price:</Col>
+                  <Col>
+                    <strong>${product.price}</strong>
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+
+              <ListGroup.Item>
+                <Row>
+                  <Col>
+                    Description:
+                    <p className='mt-2'>{product.description}</p>
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+
+              <ListGroup.Item>
+                <Row>
+                  <Col>Status:</Col>
+                  <Col>
+                    {product.countInStock > 0 ? 'In Stock' : 'Out of Stock'}
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+
+              <ListGroup.Item>
+                <Button
+                  className='btn-block'
+                  type='button'
+                  disabled={product.countInStock === 0}
+                >
+                  Add To Cart
+                </Button>
+              </ListGroup.Item>
+            </ListGroup>
+          </Card>
+        </Col>
+      </Row>
     </>
   )
 }
