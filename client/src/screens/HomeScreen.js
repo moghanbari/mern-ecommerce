d22bc22f3290064f8ed91975deca60413a3547ee
Mostafa Ghanbari <mostafa.qanbary@gmail.com>
2020-10-29 19:21:11 -0400

    Getting state values from useSelector hook and use them to show data in the component

--- client/src/screens/HomeScreen.js
+++ client/src/screens/HomeScreen.js
@@ -2,28 +2,34 @@ import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { listProducts } from '../redux/actions/productActions'
 import { Row, Col } from 'react-bootstrap'
-import axios from 'axios'
 import Product from '../components/Product'
 
 const HomeScreen = () => {
   const dispatch = useDispatch()
 
+  const productList = useSelector((state) => state.productList)
+  const { loading, products, error } = productList
+
   useEffect(() => {
     dispatch(listProducts())
   }, [dispatch])
 
-  const products = []
-
   return (
     <>
       <h1>Latest Products</h1>
-      <Row>
-        {products.map((product) => (
-          <Col sm={12} m={6} lg={4} xl={3} key={product._id}>
-            <Product product={product} />
-          </Col>
-        ))}
-      </Row>
+      {loading ? (
+        <h2>Loading...</h2>
+      ) : error ? (
+        <h3>{error}</h3>
+      ) : (
+        <Row>
+          {products.map((product) => (
+            <Col sm={12} m={6} lg={4} xl={3} key={product._id}>
+              <Product product={product} />
+            </Col>
+          ))}
+        </Row>
+      )}
     </>
   )
 }
