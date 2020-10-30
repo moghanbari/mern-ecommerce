05cb6941cd9f1328474768ba053618229939cf18
Mostafa Ghanbari <mostafa.qanbary@gmail.com>
2020-10-29 21:57:12 -0400

    Created Loader and Spinner component and use them on HomeScreen component

--- client/src/screens/HomeScreen.js
+++ client/src/screens/HomeScreen.js
@@ -3,6 +3,8 @@ import { useDispatch, useSelector } from 'react-redux'
 import { listProducts } from '../redux/actions/productActions'
 import { Row, Col } from 'react-bootstrap'
 import Product from '../components/Product'
+import Loader from '../components/layouts/Loader'
+import Message from '../components/layouts/Message'
 
 const HomeScreen = () => {
   const dispatch = useDispatch()
@@ -18,9 +20,9 @@ const HomeScreen = () => {
     <>
       <h1>Latest Products</h1>
       {loading ? (
-        <h2>Loading...</h2>
+        <Loader />
       ) : error ? (
-        <h3>{error}</h3>
+        <Message variant='danger'>{error}</Message>
       ) : (
         <Row>
           {products.map((product) => (
