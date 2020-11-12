f094aa6cb22132405bd1d1ed21a67848a58556f2
Mostafa Ghanbari <mostafa.qanbary@gmail.com>
2020-11-12 15:09:00 -0500

    Added initial CartScreen/CartPage

--- client/src/screens/CartScreen.js
+++ client/src/screens/CartScreen.js
@@ -1,6 +1,22 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { addToCart } from '../redux/actions/cartActions'
+import { Row, Col, ListGroup, Image, Form, Button, Card } from 'react-bootstrap'
+import Message from '../components/layouts/Message'
 
-const CartScreen = () => {
+const CartScreen = ({ match, location, history }) => {
+  const productId = match.params.id
+  const qty = location.search ? Number(location.search.split('=')[1]) : 1
+
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    if (productId) {
+      dispatch(addToCart(productId, qty))
+    }
+  }, [dispatch, productId, qty])
+
   return <div>Cart</div>
 }
 
